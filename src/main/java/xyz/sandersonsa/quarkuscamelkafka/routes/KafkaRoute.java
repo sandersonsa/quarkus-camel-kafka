464295cf32c1d3a9445dd6e9218052f143bafe23
Sanderson Sa <costadesa@gmal.com>
2023-08-27 15:53:42 -0300

    fix test

--- src/main/java/xyz/sandersonsa/quarkuscamelkafka/routes/KafkaRoute.java
+++ src/main/java/xyz/sandersonsa/quarkuscamelkafka/routes/KafkaRoute.java
@@ -1,14 +1,8 @@
 package xyz.sandersonsa.quarkuscamelkafka.routes;
 
-import java.util.List;
-
 import javax.enterprise.context.ApplicationScoped;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.kafka.KafkaConstants;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.jboss.logging.Logger;
 
 @ApplicationScoped
@@ -20,11 +14,6 @@ public class KafkaRoute extends RouteBuilder {
     public void configure() throws Exception {
 
 
-        // from("kafka:" + TOPIC + "?brokers=localhost:{{kafkaPort}}" +
-        //      "&groupId=A&sslKeystoreLocation=/path/to/keystore.jks&sslKeystorePassword=changeit&sslKeyPassword=changeit&securityProtocol=SSL")
-        // .to("mock:result");
-
-
         // produces messages to kafka
         from("timer:foo?period={{timer.period}}&delay={{timer.delay}}")
                 .routeId("FromTimer2Kafka")
@@ -33,26 +22,9 @@ public class KafkaRoute extends RouteBuilder {
                 .log("Message correctly sent to the topic! : \"${body}\" ");
 
         // kafka consumer
-        // from("kafka:{{kafka.topic.name}}")
-        //         .routeId("FromKafka2Seda")
-        //         .log("Received : \"${body}\"")
-        //         .to("seda:kafka-messages");
-    
-
-        // from("timer://foo?period={{period}}").setBody(constant("post message to kafka cluster topic"))
-        //         .to("kafka:{{kafka.topic.name}}?brokers={{kafka.external.bootstrap.url}}"
-        //                 + "&keySerializerClass={{kafka.key.serializer}}&serializerClass={{kafka.value.serializer}}"
-        //                 + "&securityProtocol=SSL&sslTruststoreLocation={{truststore}}"
-        //                 + "&sslTruststorePassword={{truststore.password}}")
-        //         .process(new Processor() {
-        //             @Override
-        //             public void process(Exchange exchange) throws Exception {
-        //                 List<RecordMetadata> recordMetaData1 = (List<RecordMetadata>) exchange.getIn().getHeader(KafkaConstants.KAFKA_RECORDMETA);
-        //                 for (RecordMetadata rd : recordMetaData1) {
-        //                     LOG.info("producer partition is:" + rd.partition());
-        //                     LOG.info("producer partition message is:" + rd.toString());
-        //                 }
-        //             }
-        //         });
+        from("kafka:{{kafka.topic.name}}")
+                .routeId("FromKafka2Seda")
+                .log("Received : \"${body}\"")
+                .to("seda:kafka-messages");
     }
 }
