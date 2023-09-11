8965513eaaec4ca4b8c587ee3d2e8b7104247e61
Sanderson Sa <sanderson@redhat.com>
2023-09-11 14:57:50 -0300

    adicionar large file - fix path

--- src/main/java/xyz/sandersonsa/quarkuscamelkafka/routes/KafkaRoute.java
+++ src/main/java/xyz/sandersonsa/quarkuscamelkafka/routes/KafkaRoute.java
@@ -1,7 +1,11 @@
 package xyz.sandersonsa.quarkuscamelkafka.routes;
 
+import java.io.File;
+
 import javax.enterprise.context.ApplicationScoped;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kafka.KafkaConstants;
 import org.jboss.logging.Logger;
@@ -14,20 +18,25 @@ public class KafkaRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-
         // produces messages to kafka
-        from("timer:kafka?period={{timer.period}}&delay={{timer.delay}}")
-                .routeId("FromTimer2Kafka")
+        from("timer:kafka?period={{timer.period}}&delay={{timer.delay}}").routeId("FromTimer2Kafka")
                 // .setBody().simple("Message #${exchangeProperty.CamelTimerCounter}")
                 // <to uri="language:constant:resource:classpath:/myfiles/file.xml"/>
                 // .setBody(constant("resource:classpath:files/sample.txt"))
-                .from("file:src/files/sample.txt?noop=true")
-                // .setBody().simple("resource:classpath:files/sample.txt")
+                // .from("file:src/files/sample.txt?noop=true")
+                .process(new Processor() {
+                    public void process(Exchange msg) {
+                        File file = new File("src/files/sample.txt");
+                        LOG.info("File name: " + file.getName());
+                        msg.getIn().setBody(file);
+                    }
+                })
+                // .setBody().simple("resource:classpath:src/files/sample.txt")
                 .setHeader(KafkaConstants.HEADERS, constant("MSG HEADER"))
                 .setHeader("source", constant("Outside cluste Openshift"))
                 .setHeader(KafkaConstants.KEY, constant("MSG KEY"))
                 .to("kafka:{{kafka.topic.name}}")
                 .log("Message correctly sent to the topic! : \"${body}\" ");
-        
+
     }
 }
